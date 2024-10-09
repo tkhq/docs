fa9c3e17cca54a30844ccd36e92053035ed9d9ff
Andrew Min <andrew@turnkey.io>
2024-10-09 11:44:45 -0400

    prettify

--- src/components/BugcrowdForm.js
+++ src/components/BugcrowdForm.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 function BugcrowdForm() {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://bugcrowd.com/a9e1eca7-990e-4602-bf6d-a9f70df1c2fa/external/script';
+    const script = document.createElement("script");
+    script.src =
+      "https://bugcrowd.com/a9e1eca7-990e-4602-bf6d-a9f70df1c2fa/external/script";
     script.async = true;
-    script.setAttribute('data-bugcrowd-program', 'https://bugcrowd.com/a9e1eca7-990e-4602-bf6d-a9f70df1c2fa/external/report');
-    
+    script.setAttribute(
+      "data-bugcrowd-program",
+      "https://bugcrowd.com/a9e1eca7-990e-4602-bf6d-a9f70df1c2fa/external/report",
+    );
+
     // Find the container and append the script there
-    const container = document.getElementById('bugcrowd-form-container');
+    const container = document.getElementById("bugcrowd-form-container");
     if (container) {
       container.appendChild(script);
     }
